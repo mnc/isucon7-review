65a5f264855fb98c82e73d2a476942a6d4c4e946
mnc <manchose9@gmail.com>
2017-11-21 14:41:19 +0900

    deploy nginx.conf

--- restart.sh
+++ restart.sh
@@ -6,6 +6,11 @@ now=`date +%Y%m%d-%H%M%S`
 if [ -f /var/log/nginx/access.log ]; then
   sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$now
 fi
+
+if [ -f /etc/nginx/nginx.conf ]; then
+  sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.$now
+fi
+sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo systemctl restart nginx
 
 if [ -f /var/log/mysql/slow.log ]; then
